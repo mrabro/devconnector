f3c430a126fb5b63749e205b28433a72c91105a7
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2019-01-12 23:35:36 +0500

    blank lines removed

--- validation/login.js
+++ validation/login.js
@@ -9,17 +9,14 @@ module.exports = function validateLoginInput(data) {
     if (!Validator.isEmail(data.email)) {
         errors.email = "Email is invalid";
     }
-
     if (Validator.isEmpty(data.email)) {
         errors.email = "Email field is required";
     }
-
-    if (Validator.isEmpty(data.password)) {
-        errors.password = "Password field is required";
-    }
-
     if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
         errors.password = "Password must be between 6 and 30 charactors";
+    }
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
     }
 
     return {

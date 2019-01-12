4791f35d17b0701477b76a1412aba224daa50b97
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2019-01-12 22:02:40 +0500

    routes for users and create user n login user call

---
+++ routes/api/users.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const router = express.Router();
+const gravator = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// Load User model
+const User = require("../../models/User");
+
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
+router.get('/test', (req, res) => res.json({ msg: "users work" }));
+
+// @route   POST api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({ name: 'User name is required' });
+    }
+    if (!req.body.password) {
+        return res.status(400).json({ password: 'User password is required' });
+    }
+    if (!req.body.email) {
+        return res.status(400).json({ email: 'User email is required' });
+    }
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ email: 'Email already exists' });
+            } else {
+                const avatar = gravator.url(req.body.email, {
+                    s: '200', //size
+                    r: 'pg', //rating
+                    d: 'mm' //Default
+                })
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar: avatar,
+                    password: req.body.password
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log("User register err:", err));
+                    });
+                });
+            }
+        });
+});
+
+
+// @route   POST api/users/login
+// @desc    Login user
+// @access  Public
+router.post("/login", (req, res) => {
+    if (!req.body.password) {
+        return res.status(400).json({ password: 'User password is required' });
+    }
+    if (!req.body.email) {
+        return res.status(400).json({ email: 'User email is required' });
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find user by email
+    User.findOne({ email })
+        .then(user => {
+            // check user
+            if (!user) {
+                return res.status(404).json({ email: "User email is not found" });
+            }
+
+            // check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        return res.json({ msg: 'Success' });
+                    } else {
+                        return res.status(400).json({ password: 'Password incorrect' })
+                    }
+                });
+        });
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config 
 const db = require("./config/keys").mongoURI;
@@ -10,7 +19,13 @@ mongoose
     .connect(db)
     .then(() => console.log("MongoDB Connected!"))
     .catch(err => console.log("DB error:", err));
+
 app.get('/', (req, res) => res.send("Hello!"));
+
+// use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 

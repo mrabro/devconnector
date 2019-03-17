60ed5dbf5aaf3ee9bc27aadd52445ece5e7b4a76
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2019-03-17 21:16:26 +0500

    add education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 // Load input validation
 const validateProfileInput = require("../../validation/profile");
 const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 
 // Load models
 const User = require("../../models/User");
@@ -100,7 +101,7 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
     Profile.findOne({ user: req.user.id })
         .then(profile => {
             const newExp = {
-                    title: req.body.company,
+                    title: req.body.title,
                     location: req.body.location,
                     from: req.body.from,
                     company: req.body.company,
@@ -110,6 +111,32 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
                 }
                 // Add to exp array
             profile.experience.unshift(newExp);
+            profile.save().then(profile => res.json(profile));
+        });
+});
+
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            const newEdu = {
+                    school: req.body.school,
+                    degree: req.body.degree,
+                    fieldofstudy: req.body.fieldofstudy,
+                    from: req.body.from,
+                    to: req.body.to,
+                    current: req.body.current,
+                    description: req.body.description
+                }
+                // Add to exp array
+            profile.education.unshift(newEdu);
             profile.save().then(profile => res.json(profile));
         });
 });

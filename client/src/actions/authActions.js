25093ad5f846d1e1252c6947f4f87ac5e4ad9294
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2019-03-25 23:10:59 +0500

    jwtToken Decoder module added, Login action and reducer created

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +14,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get user Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save to Local Storage
+      const { token } = res.data;
+      // Set token to local storage
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth Header
+      setAuthToken(token);
+      // Decode Token to get user data
+      const decoded = jwt_decode(token);
+      // Set Current User
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in User
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };

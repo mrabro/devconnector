616fb3cb856f346ab88d910fa12bf0c967a64326
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2019-04-01 23:24:00 +0500

    created session if user is signed in and jwtToken is stored in localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './store';
+import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -9,6 +12,16 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import "./App.css";
+
+// Check for Token
+if (localStorage.jwtToken) {
+  // Set the Auth Token Header Auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode Token to get user data
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
